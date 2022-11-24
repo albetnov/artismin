47d1bdce201a2efb0811f5b3fb88c84a1778d58e
albetnov <ryxinfrvr@gmail.com>
2022-11-24 10:45:07 +0700

    fix lazy loaded fails

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
+import Loading from "./Components/Loading";
 import Routes from "./Routes";
 
 function App() {
-  return <RouterProvider router={Routes} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={Routes} />
+    </Suspense>
+  );
 }
 
 export default App;
